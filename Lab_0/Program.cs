d498d214e839fcef366f725aa1e2c1ce27495cc5
SHLAPAK MICHAEL RODIONOVICH <misha.shlapak921@gmail.com>
2024-11-15 17:59:59 +0700

    Добавление комментариев

--- Lab_0/Program.cs
+++ Lab_0/Program.cs
@@ -7,8 +7,10 @@ public class Program
         Console.Write("Введите число: ");
         int number = Convert.ToInt32(Console.ReadLine());
 
+        // Проверяем, является ли число четным
         bool isEven = number % 2 == 0;
 
+        // Выводим результат
         if (isEven)
         {
             Console.WriteLine("Число четное");
