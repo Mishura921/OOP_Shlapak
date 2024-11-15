5c50ed0836cab4f365b92c12180b9e6081341ccb
SHLAPAK MICHAEL RODIONOVICH <misha.shlapak921@gmail.com>
2024-11-15 17:45:20 +0700

    Базовая программа

---
+++ Lab_0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Число четное");
+        }
+        else
+        {
+            Console.WriteLine("Число нечетное");
+        }
+    }
+}
