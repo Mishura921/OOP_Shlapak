e87946d385dd14584a4859364e8f74c7dd670354
Michael <misha.shlapak921@gmail.com>
2025-02-13 11:30:59 +0700

    feature: реализован класс Person, содержащий имя, фамилию, возраст, пол

---
+++ Laboratornaya_1/PersonsClassLibrary/Gender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsClassLibrary
+{
+    enum Gender
+    {
+        /// <summary>
+        /// Женский
+        /// </summary>
+        Female,
+        /// <summary>
+        /// Мужской
+        /// </summary>
+        Male,
+        /// <summary>
+        /// Другой
+        /// </summary>
+        Other,
+        /// <summary>
+        /// Неизвестно
+        /// </summary>
+        Unknown
+    }
+}
---
+++ Laboratornaya_1/PersonsClassLibrary/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsClassLibrary
+{
+    class Person
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        private string _name;
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        private string _surname;
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        private int _age;
+        /// <summary>
+        /// Минимальный предусматриваемый возраст
+        /// </summary>
+        private const int MinAge = 0;
+        /// <summary>
+        /// Максимальный предусматриваемый возраст
+        /// </summary>
+        private const int MaxAge = 120;
+        /// <summary>
+        /// Гендер
+        /// </summary>
+        private Gender _gender;
+    }
+}
