48539d95ef19ab3034c6625792b32ec942022175
Michael <misha.shlapak921@gmail.com>
2025-02-19 22:26:03 +0700

    feature(Person): добавлен класс Person, содержащий: имя, фамилию, возраст, пол, конструктор класса. Добавлены методы для валидации вводимых данных. Добавлены XML-комментарии.

--- Laboratornaya_1/PersonsClassLibrary/Person.cs
+++ Laboratornaya_1/PersonsClassLibrary/Person.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PersonsClassLibrary
 {
     class Person
     {
+        // Поля класса Person: определяем их как приватные, доступ к ним будем осуществлять с помощью методов
         /// <summary>
         /// Имя
         /// </summary>
@@ -32,5 +36,145 @@ namespace PersonsClassLibrary
         /// Гендер
         /// </summary>
         private Gender _gender;
+
+        // Методы класса Person. Позволяютт получить доступ к приватным полям. Так реализуем инкапсуляцию. Безопасность
+        /// <summary>
+        /// Вводимое имя
+        /// </summary>
+        public string InputName
+        {
+            get => _name;
+
+            set
+            {
+                LanguageValidation(value);
+                _name = EditRegister(value);
+
+                if (_surname != null)
+                {
+                    NameSurnameValidation();
+                }
+            }
+        }
+        /// <summary>
+        /// Вводимая фамилия
+        /// </summary>
+        public string InputSurname
+        {
+            get => _surname;
+
+            set
+            {
+                LanguageValidation(value);
+                _surname = EditRegister(value);
+
+                if (_surname != null)
+                {
+                    NameSurnameValidation();
+                }
+            }
+        }
+        /// <summary>
+        /// Передаваемый возраст
+        /// </summary>
+        public int InputAge
+        {
+            get => _age;
+            set
+            {
+                try
+                {
+                    if (value <= MinAge || value >= MaxAge)
+                        throw new IndexOutOfRangeException($"Возраст должен быть в диапазоне: [{MinAge} - {MaxAge}].");
+
+                    _age = value;
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Передаваемый пол
+        /// </summary>
+        public Gender InputGender
+        {
+            get => _gender;
+            set => _gender = value;
+        }
+        /// <summary>
+        /// Конструктор класса Person
+        /// </summary>
+        public Person
+            (string name, string surname, int age, Gender gender)
+        {
+            InputName = name; // this.name = name ???
+            InputSurname = surname;
+            InputAge = age;
+            InputGender = gender;
+        }
+
+        /// <summary>
+        /// Валидация языка. Функция оределяет, какому языку соответствует переданная строка name
+        /// </summary>
+        private static Language LanguageValidation(string name)
+        {
+            var engLetters = new Regex(@"^[A-z]+(-[A-z])?[A-z]*$");
+            var ruLetters = new Regex(@"^[А-я]+(-[А-я])?[А-я]*$");
+
+            try
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new ArgumentException("Имя не может являться null или быть пустым!");
+                }
+
+                if (engLetters.IsMatch(name))
+                {
+                    return Language.English;
+                }
+
+                if (ruLetters.IsMatch(name))
+                {
+                    return Language.Russian;
+                }
+
+                throw new ArgumentException("Некорректный ввод, повторите!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return Language.Other;
+            }
+        }
+        /// <summary>
+        /// Функция сравнивает, на одном ли языке введены имя и фамилия
+        /// </summary>
+        private void NameSurnameValidation()
+        {
+            if ((string.IsNullOrEmpty(InputName) == false)
+                && (string.IsNullOrEmpty(InputSurname) == false))
+            {
+                var nameLanguage = LanguageValidation(InputName);
+                var surnameLanguage = LanguageValidation(InputSurname);
+
+                if (nameLanguage != surnameLanguage)
+                {
+                    throw new FormatException("Имя и фамилия должны быть на одном языке");
+                }
+            }
+        }
+        /// <summary>
+        /// Функция меняет первую букву на заглавную, оставляя остальные строчными. Прим.: иЛьЯ - > Илья
+        /// </summary>
+        private static string EditRegister(string word)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.
+                ToTitleCase(word.ToLower());
+        }
+
     }
-}
+} 
+
