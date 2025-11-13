51fc9f9c84656fb910744a6c9bd1193cce08fff3
Michael <misha.shlapak921@gmail.com>
2025-11-13 15:25:17 +0700

    style: RSDN

--- ConsoleLoader/Program.cs
+++ ConsoleLoader/Program.cs
@@ -26,36 +26,36 @@ namespace ConsoleLoader
                 var consoleKey = Console.ReadLine();
                 switch (consoleKey)
                 {   
-                    //TODO: RSDN
+                    //TODO: RSDN+
                     case "1":
-                        {
-                            GetSquareInfo(AddConsoleFigure.
-                                GetNewTriangleFromKeyboard());
-                            break;
-                        }
+                    {
+                        GetSquareInfo(AddConsoleFigure.
+                            GetNewTriangleFromKeyboard());
+                        break;
+                    }
                     case "2":
-                        {
-                            GetSquareInfo(AddConsoleFigure.
-                                GetNewRectangleFromKeyboard());
-                            break;
-                        }
+                    {
+                        GetSquareInfo(AddConsoleFigure.
+                            GetNewRectangleFromKeyboard());
+                        break;
+                    }
                     case "3":
-                        {
-                            GetSquareInfo(AddConsoleFigure.
-                                GetNewCircleFromKeyboard());
-                            break;
-                        }
+                    {
+                        GetSquareInfo(AddConsoleFigure.
+                            GetNewCircleFromKeyboard());
+                        break;
+                    }
                     case "4":
-                        {
-                            Environment.Exit(0);
-                            break;
-                        }
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
                     default:
-                        {
-                            Console.WriteLine("Ошибка: " +
-                                "ожидается целое число от 1 до 4.");
-                            break;
-                        }
+                    {
+                        Console.WriteLine("Ошибка: " +
+                            "ожидается целое число от 1 до 4.");
+                        break;
+                    }
                 }
             }
         }
